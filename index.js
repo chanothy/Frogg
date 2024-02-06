9f5b555442d2de0c175923505b5a1ad114421591
Tim Chan <timchatan@gmail.com>
2024-02-06 18:31:25 -0500

    first command

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const {Client} = require("discord.js");
+const {Client, Events, GatewayIntentBits} = require("discord.js");
 const {token} = require("./config.json");
 
-const client = new Client();
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, readyClient => {
+    // readyClient references self
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+})
 
 client.login(token);
